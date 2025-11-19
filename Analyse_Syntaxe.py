ff435e4dd5545c4c8ed24cdcbc6d751057bd1583
PEGURIER-C <clement.pegurier@etu.umontpellier.fr>
2025-11-19 15:39:55 +0100

    Update Analyse_Syntaxe.py

--- Analyse_Syntaxe.py
+++ Analyse_Syntaxe.py
@@ -5,58 +5,56 @@ import numpy as np
 import re 
 
 def LigneCorrecte(ligne) :
-  global VAR
-	global point
-  if re.search('SetFactory("OpenCASCADE");', ligne):
-    return True
-  if re.search('//+', ligne):
-    return True
-  if re.search('.*=.*;', ligne):
-    VAR.append(re.search('(.*)=.*', ligne)) 
-    return True
- 
-	if re.search('Point *\(.*\) *= *\{(.*, ){3} 1.0\};', ligne):
-		return True
+    global VAR
+    if re.search('SetFactory\\("OpenCASCADE"\\);', ligne):
+        return True
+    if re.search('//+', ligne):
+        return True
+    if re.search('.*=.*;', ligne):
+        VAR.append(re.search('(.*)=.*', ligne)) 
+        return True
+    if re.search('Point *\\(.*\\) *= *\\{(.*, ){3} 1.0\\};', ligne):
+        return True
+    
+    if re.search('Circle *\\(.*\\) *= *\\{(.*, ){2}.*\\};', ligne):
+        return True
 	
-	if re.search('Circle *\(.*\) *= *\{(.*, ){2}.*\};', ligne):
-		return True
+    if re.search('Line *\\(.*\\) *= *\\{.*, .*\\};', ligne):
+        return True
 	
-	if re.search('Line *\(.*\) *= *\{.*, .*\};', ligne):
-		return True
+    if re.search('Curve Loop *\\(.*\\) *= *\\{(.*, )+ .*\\};', ligne):
+        return True
+
+    if re.search('Plane Surface *\\(.*\\) *= *\\{(.*, )* .*\\};', ligne):
+        return True
 	
-	if re.search('Curve Loop *\(.*\) *= *\{(.*, )+ .*\};', ligne):
-		return True
-
-  if re.search('Plane Surface *\(.*\) *= *\{(.*, )* .*\};', ligne):
-		return True
+    if re.search('Ruled Surface *\\(.*\\) *= *\\{(.*, )* .*\\};', ligne):
+        return True
 		
-  if re.search('Ruled Surface *\(.*\) *= *\{(.*, )* .*\};', ligne):
-		return True
+    if re.search('Physical Curve *\\(.*\\) *= *\\{(.*, )* .*\\};', ligne):
+        return True
+
+    if re.search('Physical Surface *\\(.*\\) *= *\\{(.*, )* .*\\};', ligne):
+        return True
 		
-  if re.search('Physical Curve *\(.*\) *= *\{(.*, )* .*\};', ligne):
-		return True
+    if re.search('Cylinder *\\(.*\\) *= *\\{(.*, ){6,7} .*\\};', ligne):
+        return True
 
-	if re.search('Physical Surface *\(.*\) *= *\{(.*, )* .*\};', ligne):
-		return True
-		
-  if re.search('Cylinder *\(.*\) *= *\{(.*, ){6,7} .*\};', ligne):
-		return True
-				
-  if re.search('Box *\(.*\) *= *\{(.*, ){5} .*\};', ligne):
-		return True
-				
-  if re.search('Physical Volume *\(.*\) *= *\{(.*, )* .*\};', ligne):
-		return True
-				
-  if re.search('Extrude *\{(.*, ){2}.*\} *\{Surface\{.*\};\}', ligne):
-		return True
-					
-  if re.search('BooleanUnion *(\{.*\{.*\}; .*; /}){2,}', ligne):
-		return True
+    if re.search('Box *\\(.*\\) *= *\\{(.*, ){5} .*\\};', ligne):
+        return True
+
+    if re.search('Physical Volume *\\(.*\\) *= *\\{(.*, )* .*\\};', ligne):
+        return True
+
+    if re.search('Extrude *\\{(.*, ){2}.*\\} *\\{Surface\\{.*\\};\\}', ligne):
+        return True
+
+    if re.search('BooleanUnion *(\\{.*\\{.*\\}; .*; \\}){2,}', ligne):
+        return True
+    return False
 
 
-
-with open('Plongeoir.geo', 'r') as file:
+with open('plongeoir.geo', 'r') as file:
 	lignes = file.readlines()
   
 cpt = 0
@@ -64,7 +62,6 @@ VAR=[]
 point=[]
 
 for ligne in lignes :
-  cpt += 1
-  if LigneCorrecte==False : 
-    print('la ligne numéro ' + str(cpt) + ' est fausse' )
-
+    cpt += 1
+    if LigneCorrecte(ligne)==False : 
+        print('la ligne numéro ' + str(cpt) + ' est fausse' )

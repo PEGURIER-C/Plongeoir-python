da9c24eda79685a598165cd7cb97211cd4b4699d
sachasorlut3-blip <sacha.sorlut3@gmail.com>
2025-11-19 16:25:41 +0100

    Update Main_Diff_Pos.py

--- Main_Diff_Pos.py
+++ Main_Diff_Pos.py
@@ -3,6 +3,28 @@ import subprocess
 import pandas as pd
 import numpy as np
 LARG=0.6
+Valeurs=pd.DataFrame({})
+val1=[]
+val2=[]
+val3=[]
+with open("val_positions","r") as fichier :
+    cpt=0
+    for ligne in fichier: 
+        cpt+=1
+        if cpt==1: # il faut qu'il y ait 3 variables, il ne vérifie pas la forme du fichier (ce qui peut tout faire capoter)
+            m=re.search('((.*)\\:){0,2}(.*)',ligne)
+            var1 = m.group(0)
+            var2 = m.group(1)
+            var3 = m.group(2)
+        else :
+            m=re.search('((.*)\\:){0,2}(.*)',ligne)
+            val1.append(m.group(0))
+            val2.append(m.group(1))    
+            val3.append(m.group(2))
+Valeurs[var1]= val1
+Valeurs[var2]= val2
+Valeurs[var3]= val3
+
 positions=np.array([0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,0.95])
 positions = positions*LARG
 res_fleche = []

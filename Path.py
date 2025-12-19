24ef13c9d69fc8c9c9605febfab224880c6d4fc6
sachasorlut3-blip <sacha.sorlut3@gmail.com>
2025-12-19 13:02:39 +0100

    Add initial Path.py with file path variables

---
+++ Path.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+
+dgibiPath = "Plongeoir.dgibi" # écrire le chemin absolu ou relatif de votre .dgibi entre guillement
+geoPath = "Plongeoir.geo"  # écrire le chemin absolu ou relatif du .geo associé entre guillement
